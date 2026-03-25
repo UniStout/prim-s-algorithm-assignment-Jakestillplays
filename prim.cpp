8cca97ff4e512082ae060824cf5e760b2c7e42df
Jakestillplays <128544537+Jakestillplays@users.noreply.github.com>
2026-03-25 13:01:40 -0500

    Added input example and definition

--- prim.cpp
+++ prim.cpp
@@ -1,1 +1,12 @@
-//All procedures including main
+#include <iostream>
+using namespace std;
+
+// Input file example:
+// ------------------
+// 3 (vertices/V)
+// 3 (edges/E)
+// 0 1 4 (0 = current node, 1 = connected node, 4 = weight)
+// 1 2 5
+// 0 2 6
+// ------------------
+
